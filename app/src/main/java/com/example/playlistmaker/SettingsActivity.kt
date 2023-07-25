e6b0b7bd6a8f932517107ae654945dc997b9f775
vochik <vochik@live.ru>
2023-07-25 22:45:17 +0300

    Домашняя работа по спринту №9

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
@@ -13,6 +15,32 @@ class SettingsActivity : AppCompatActivity() {
         val backButton = findViewById<ImageView>(R.id.buttonBack)
         backButton.setOnClickListener {
             finish()
+        }
+
+        val buttonShare = findViewById<FrameLayout>(R.id.buttonShare)
+        buttonShare.setOnClickListener{
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, "https://practicum.yandex.ru/profile/android-developer/")
+            intent.type = "text/plain"
+            startActivity(intent)
+        }
+
+        val buttonSupport = findViewById<FrameLayout>(R.id.buttonSupport)
+        buttonSupport.setOnClickListener{
+            val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+            val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val shareIntent = Intent(Intent.ACTION_SENDTO)
+            shareIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("v.klimushin@ya.ru"))
+            shareIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
+            shareIntent.putExtra(Intent.EXTRA_TEXT, message)
+            shareIntent.data = Uri.parse("mailto:")
+            startActivity(shareIntent)
+        }
+
+        val buttonTerms = findViewById<FrameLayout>(R.id.buttonTerms)
+        buttonTerms.setOnClickListener{
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/")))
         }
 
     }
